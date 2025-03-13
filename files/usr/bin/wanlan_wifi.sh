85a0bf4a9e8bd6f68712c3f2242a4b1f469336d2
sinegar <peter@perenyi.net>
2025-03-13 12:09:16 +1100

    enable captive portal

--- files/usr/bin/wanlan_wifi.sh
+++ files/usr/bin/wanlan_wifi.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# use dhcp on wan (the below wifi)
 uci set network.wan=interface
 uci set network.wan.proto='dhcp'
 
+# cleanu default/stock wifi
 uci del wireless.default_radio0
 
+# connect to wifi as new wan
 uci set wireless.wifinet1=wifi-iface
 uci set wireless.wifinet1.device='radio0'
 uci set wireless.wifinet1.mode='sta'
@@ -13,7 +16,12 @@ uci set wireless.wifinet1.encryption='psk2'
 uci set wireless.wifinet1.ssid="$1"
 uci set wireless.wifinet1.key="$2"
 
+# disable captive portal
+uci set firewall.captive.enabled='0'
+
+uci commit firewall
 uci commit network
 uci commit wireless
 
+service firewall restart
 service network reload
